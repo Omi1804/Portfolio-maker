0cafe0dfc81ae5c5e701945ce3fe45eb3d11dc46
Pragya <pragya142018@gmail.com>
2023-05-28 00:42:31 +0530

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="container">
+      <h1 className="centered larger-font">
+        <h3>MY</h3>
+        <span className="animation">
+          <span>P</span>
+          <span>O</span>
+          <span>R</span>
+          <span>T</span>
+          <span>F</span>
+          <span>O</span>
+          <span>L</span>
+          <span>I</span>
+          <span>O</span>
+        </span>
+      </h1>
+      <p className="centered larger-font">I AM A <span className="animation">
+       <span>B</span>
+       <span>L</span>
+       <span>O</span>
+       <span>C</span>
+       <span>K</span>
+       <span>C</span>
+       <span>H</span>
+       <span>A</span>
+       <span>I</span>
+       <span>N</span>
+ </span>  and <span className="animation">
+          
+     <span>F</span>
+       <span>R</span>
+       <span>O</span>
+       <span>N</span>
+       <span>T</span>
+       <span>-</span>
+       <span>E</span>
+       <span>N</span>
+       <span>D</span>
+       <span>-</span>
+       <span>D</span>
+       <span>E</span>
+       <span>V</span>
+       <span>E</span>
+       <span>L</span>
+       <span>O</span>
+       <span>P</span>
+       <span>E</span>
+       <span>R</span>
+        
+            
+            </span>.</p>
+      <p className="centered">I have experience developing decentralized applications and smart contracts on various blockchain platforms, including Ethereum and Binance Smart Chain.</p>
+      <p className="centered">I am proficient in React, JavaScript, and Solidity, and I enjoy working on projects that combine my skills in blockchain and front-end development.</p>
+      <p className="centered">Please feel free to explore my portfolio and contact me if you have any questions or if you're interested in working together.</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <Link to="/" className="nav-links">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/skills" className="nav-links">
+              Skills
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/projects" className="nav-links">
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-links">
+              Contact Me
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
